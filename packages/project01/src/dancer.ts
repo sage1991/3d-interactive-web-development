8b1d7dba453079951de9bca1f3b9451adfdd7a20
harry <shsage91@gmail.com>
2025-03-07 00:47:11 +0900

    Improve dancer example

--- packages/project01/src/dancer.ts
+++ packages/project01/src/dancer.ts
@@ -3,13 +3,12 @@ import "./style.css"
 import { aspect, isMesh } from "@example/common"
 import {
   AnimationMixer,
-  AxesHelper,
   Clock,
   DoubleSide,
   LoopPingPong,
   Mesh,
   MeshStandardMaterial,
-  PCFSoftShadowMap,
+  Object3D,
   PerspectiveCamera,
   PlaneGeometry,
   Raycaster,
@@ -22,33 +21,63 @@ import {
 } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+import { clone } from "three/examples/jsm/utils/SkeletonUtils.js"
 
+const SPOT_LIGHT_COUNT = 10
+const SPOT_LIGHT_COLORS = [0xff0000, 0x00ff00, 0x0000ff]
+const AUDIENCE_NUMBER = 40
+const FLOOR_SIZE = 30
+const FLOOR_HALF_SIZE = FLOOR_SIZE / 2
 const root = document.body
 
 const scene = new Scene()
-scene.add(new AxesHelper(10))
 
 const light = new SpotLight(0xffffff, 10, undefined, Math.PI / 6, 1, 0.5)
 light.position.set(0, 10, 0)
 light.castShadow = true
-light.shadow.mapSize.set(4096, 4096)
 scene.add(light)
 
+const spotLights = Array.from({ length: SPOT_LIGHT_COUNT }, () => {
+  const index = Math.floor(Math.random() * SPOT_LIGHT_COLORS.length)
+  const light = new SpotLight(
+    SPOT_LIGHT_COLORS[index],
+    10,
+    undefined,
+    Math.PI / 15,
+    1,
+    0.5
+  )
+  light.position.set(0, 10, 0)
+
+  const target = new Object3D()
+  const theta = Math.random() * 2 * Math.PI
+  const r = Math.floor(Math.random() * FLOOR_HALF_SIZE)
+  const direction = Math.random() > 0.5 ? -1 : 1
+
+  target.position.set(r * Math.cos(theta), 0, r * Math.sin(theta))
+  light.target = target
+
+  scene.add(light)
+  scene.add(target)
+
+  return { light, target, r, direction }
+})
+
 const positions: [x: number, y: number, z: number][] = [
-  [20, 0.5, 0],
-  [-20, 0.5, 0],
-  [0, 0.5, 20],
-  [0, 0.5, -20]
+  [FLOOR_HALF_SIZE, 0.5, 0],
+  [-FLOOR_HALF_SIZE, 0.5, 0],
+  [0, 0.5, FLOOR_HALF_SIZE],
+  [0, 0.5, -FLOOR_HALF_SIZE]
 ]
 positions.forEach((position) => {
-  const rectAreaLight = new RectAreaLight(0x0000ff, 1, 40, 1)
+  const rectAreaLight = new RectAreaLight(0x0000ff, 5, FLOOR_SIZE, 1)
   rectAreaLight.position.set(...position)
   rectAreaLight.lookAt(0, 0, 0)
   scene.add(rectAreaLight)
 })
 
 const floor = new Mesh(
-  new PlaneGeometry(40, 40),
+  new PlaneGeometry(FLOOR_SIZE, FLOOR_SIZE),
   new MeshStandardMaterial({
     color: 0xffffff,
     side: DoubleSide
@@ -60,23 +89,43 @@ floor.receiveShadow = true
 scene.add(floor)
 
 const loader = new GLTFLoader()
-const dancer = await loader.loadAsync("/dancer.glb")
-dancer.scene.position.set(0, 1.66, 0)
-dancer.scene.scale.set(0.02, 0.02, 0.02)
-dancer.scene.receiveShadow = true
-dancer.scene.castShadow = true
-dancer.scene.traverse((object) => {
+const { scene: dancer, animations } = await loader.loadAsync("/dancer.glb")
+dancer.position.set(0, 1.66, 0)
+dancer.scale.set(0.02, 0.02, 0.02)
+dancer.receiveShadow = true
+dancer.castShadow = true
+dancer.traverse((object) => {
   if (isMesh(object)) {
     object.castShadow = true
     object.receiveShadow = true
   }
 })
-scene.add(dancer.scene)
+scene.add(dancer)
 
-light.target = dancer.scene
+const audience = Array.from({ length: AUDIENCE_NUMBER }, () => {
+  return [
+    Math.floor(Math.random() * FLOOR_HALF_SIZE) *
+      (Math.random() > 0.5 ? -1 : 1),
+    1.66,
+    Math.floor(Math.random() * FLOOR_HALF_SIZE) * (Math.random() > 0.5 ? -1 : 1)
+  ]
+}).map(([x, y, z]) => {
+  const person = clone(dancer)
+  const mixer = new AnimationMixer(person)
+  person.position.set(x, y, z)
+  return { person, mixer }
+})
 
-const mixer = new AnimationMixer(dancer.scene)
-const actions = dancer.animations.map((animation) => {
+audience.forEach(({ person, mixer }) => {
+  scene.add(person)
+  const index = Math.floor(Math.random() * animations.length)
+  mixer.clipAction(animations[index]).setLoop(LoopPingPong, Infinity).play()
+})
+
+light.target = dancer
+
+const mixer = new AnimationMixer(dancer)
+const actions = animations.map((animation) => {
   const action = mixer.clipAction(animation)
   action.setLoop(LoopPingPong, Infinity)
   return action
@@ -84,8 +133,8 @@ const actions = dancer.animations.map((animation) => {
 
 const destination = new Vector3(0, 1.66, 0)
 const rayCaster = new Raycaster()
-root.addEventListener("mousedown", (e: MouseEvent) => {
-  if (e.button !== 2) {
+root.addEventListener("pointerdown", (e: PointerEvent) => {
+  if (e.pointerType === "mouse" && e.button !== 2) {
     return
   }
   const { clientX, clientY } = e
@@ -102,16 +151,16 @@ root.addEventListener("mousedown", (e: MouseEvent) => {
 
 const camera = new PerspectiveCamera(60, aspect())
 camera.aspect = aspect()
-camera.position.set(20, 20, 20)
-camera.lookAt(0, 0, 0)
+camera.position.set(FLOOR_HALF_SIZE, FLOOR_HALF_SIZE, FLOOR_HALF_SIZE)
+camera.lookAt(dancer.position.x, dancer.position.y, dancer.position.z)
 
 const controls = new OrbitControls(camera, root)
 controls.enableDamping = true
+controls.enablePan = false
 
 const renderer = new WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
-renderer.shadowMap.type = PCFSoftShadowMap
 root.appendChild(renderer.domElement)
 
 window.addEventListener("resize", () => {
@@ -124,17 +173,20 @@ const delta = new Vector3()
 
 const clock = new Clock()
 const render = () => {
-  dancer.scene.lookAt(destination)
+  dancer.lookAt(destination)
+  camera.lookAt(dancer.position)
+
   delta
-    .subVectors(destination, dancer.scene.position)
+    .subVectors(destination, dancer.position)
     .normalize()
-    .multiplyScalar(0.05)
+    .multiplyScalar(0.08)
+
   const isMove =
-    Math.abs(dancer.scene.position.x - destination.x) > 0.05 &&
-    Math.abs(dancer.scene.position.z - destination.z) > 0.05
+    Math.abs(dancer.position.x - destination.x) > 0.05 &&
+    Math.abs(dancer.position.z - destination.z) > 0.05
   if (isMove) {
-    dancer.scene.position.x += delta.x
-    dancer.scene.position.z += delta.z
+    dancer.position.x += delta.x
+    dancer.position.z += delta.z
     actions.forEach((action, index) => {
       if (index !== 3 && action.isRunning()) {
         action.stop()
@@ -154,10 +206,41 @@ const render = () => {
       }
     })
   }
+
+  const deltaT = clock.getDelta()
+
+  spotLights.forEach(({ target, r, direction }) => {
+    let theta = Math.atan(target.position.z / Math.abs(target.position.x))
+    if (Math.sign(target.position.x) === -1) {
+      theta +=
+        theta > 0 ? 2 * (Math.PI / 2 - theta) : -2 * (Math.PI / 2 + theta)
+    }
+    target.position.x = r * Math.cos(theta + direction * 2 * deltaT)
+    target.position.z = r * Math.sin(theta + direction * 2 * deltaT)
+  })
+
+  mixer.update(deltaT)
+  audience.forEach(({ mixer }) => mixer.update(deltaT))
   controls.update()
-  mixer.update(clock.getDelta())
   renderer.render(scene, camera)
   requestAnimationFrame(render)
 }
 
 requestAnimationFrame(render)
+
+const audio = document.createElement("audio")
+audio.autoplay = true
+audio.loop = true
+audio.volume = 1
+audio.style.visibility = "hidden"
+
+const source = document.createElement("source")
+source.src = "club.mp3"
+source.type = "audio/mp3"
+
+audio.appendChild(source)
+document.body.appendChild(audio)
+
+root.addEventListener("pointerdown", () => {
+  audio.paused && audio.play()
+})
