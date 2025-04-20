fc0c776c157bfdacb9dcfd858844c254318168d1
harry <shsage91@gmail.com>
2025-04-20 23:35:28 +0900

    Update project configurations and Init new package "ch04"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,32 @@
 import js from "@eslint/js"
 import prettier from "eslint-plugin-prettier/recommended"
-import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
 import globals from "globals"
-import ts from "typescript-eslint"
+import tseslint from "typescript-eslint"
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
   { ignores: ["dist"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  react.configs.flat.recommended,
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    }
+  },
   {
     plugins: {
       "simple-import-sort": simpleImportSort
@@ -28,4 +42,4 @@ export default [
       "@typescript-eslint/no-unused-expressions": "off"
     }
   }
-]
+)
